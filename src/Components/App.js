d35c80a9392dc58be1f61b9f1dc3a1194dc547ea
Jay <psatdoctor@naver.com>
2019-09-08 10:34:13 +0900

    CSS in Router Part3
    <a href>를 활용한 링크는 페이지간 링크이기 떄문에 라우터를 통한 데이터 불러온 상황 간의 링크는 <SLink>를 사용하는 것이 좋다

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Router from './Router';
-import Header from './Header/Header';
+
 
 // index에서 초기에 아래 App이 렌더링되는 것까지 초기 작업으로 진행됨.
 // App이 아래 앱 하에 있는 콤포넌트인 라우터까지 실행시키게 된다. 
@@ -15,7 +15,7 @@ class App extends Component {
     return (
 // return에는 화면상에 보여지는 부분이 나와야 한다. html로 작성. cf) javaScript로 작업하는 건 ~모양으로 불러온다. 
         <>
-          <Header />
+
           <Router />
         </>
     );
---
+++ src/Components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header``;
+
+const List = styled.ul``;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
+export default () => (
+    <header>
+        <List>
+            <Item>
+                <SLink to="/">Movies</SLink>
+            </Item>
+            <Item>
+                <SLink to ="/tv">Tv</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">Search</SLink>
+            </Item>
+        </List>
+
+    </header>
+);
