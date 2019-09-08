2f48ab794ce4bbbd606ea0c287db74a181de0a3a
Jay <psatdoctor@naver.com>
2019-09-08 10:17:19 +0900

    React Router Header Setting

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {HashRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "./../Routes/Home";
 import Tv from "./../Routes/Tv";
 import Search from "./../Routes/Search";
 
 export default () => (
     <Router>
-        <>
+        <Switch>
         {/* path는 도메인 요청, component는 그 입력에 따라 실행되는 주소(import한 값). 도메인 뒤에 아래를 입력하면, 해당 파일을 불러온다. */}
-            <Route path="/" exact component={Home}/>
-            <Route path="/tv" exact component={Tv}/>
-            <Route path="/search" exact component={Search}/>
-        </>
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={Tv} />
+            <Route path="/search" exact component={Search} />
+            <Redirect from = "*" to ="/" />
+        </Switch>
     </Router>
 );
